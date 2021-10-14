4311c0cd6025f1292dbad9b5451a9c61a15c6dcc
Vighnesh Unnithan <vighnu2255@gmail.com>
2021-10-14 13:30:26 -0400

    Creating empty user and artist id info databases

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
 import flask
 import os
 from myApp import fetch_data
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("Database_Uri")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(120), unique = True)
+
+
+class Artist_Info(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(120))
+    artist_id = db.Column(db.String(80))
+
+db.create_all()
+
+'''
+@app.route("/signup")
+def signup():
+
+
+@app.route("/signup", methods = ["POST"])
+def signup_post():
+
+
+@app.route("/login")
+def login():
+
+
+@app.route("/login_post", methods = ["POST"])
+def login_post():
+
+'''
 @app.route("/")
-
 def index():
     data = fetch_data()
     return flask.render_template(
